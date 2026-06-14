656658fe5d69a156d21edb53f35bc9e2a3437f40
Darkmintis <darkmintis@gmail.com>
2026-06-14 14:05:52 +0545

    chore(di): register PlaylistService and FavoritesService in container

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -2,7 +2,9 @@ import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path_provider/path_provider.dart';
 import '../../features/playlist/repositories/playlist_repository.dart';
+import '../../features/playlist/services/playlist_service.dart';
 import '../../features/favorites/repositories/favorites_repository.dart';
+import '../../features/favorites/services/favorites_service.dart';
 import '../constants/app_constants.dart';
 import '../utils/logger.dart';
 
@@ -15,7 +17,13 @@ Future<void> initDependencies() async {
     await Hive.openBox(AppConstants.hiveBoxName);
 
     sl.registerLazySingleton<PlaylistRepository>(() => PlaylistRepository());
+    sl.registerLazySingleton<PlaylistService>(
+      () => PlaylistService(sl<PlaylistRepository>()),
+    );
     sl.registerLazySingleton<FavoritesRepository>(() => FavoritesRepository());
+    sl.registerLazySingleton<FavoritesService>(
+      () => FavoritesService(sl<FavoritesRepository>()),
+    );
 
     AppLogger.info('Dependencies initialized successfully');
   } catch (e) {
